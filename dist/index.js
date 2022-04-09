f830c4edaa1d4bdffe9c5204a9afe8da499d32b5
Jaron <jaronvwell@gmaill.com>
2022-04-09 14:23:20 +0200

    initialised database

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var port = 3000;
+app.listen(port, function () {
+    console.log("started on http://localhost:".concat(port));
+});
+//# sourceMappingURL=index.js.map
