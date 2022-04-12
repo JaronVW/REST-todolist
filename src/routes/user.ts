e8a2859ebee7f1dffc6122c5ada82523b072255c
Jaron <jaronvwell@gmaill.com>
2022-04-12 18:10:10 +0200

    signing up user call in progress

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,31 +1,58 @@
 import { PrismaClient } from "@prisma/client";
 const router = require("express").Router();
+import { check, validationResult } from "express-validator";
+const { user } = new PrismaClient();
 
-const { user } = new PrismaClient()
 
-router.get('/', async(req: any,res: any)=>{
-    const users = await user.findMany({
-        select: {
-            id: true,
-            username: true
-        }
-    })
-    res.json(users)
+
+router.get("/", async (req: any, res: any) => {
+  const users = await user.findMany({
+    select: {
+      id: true,
+      username: true,
+    },
+  });
+  res.json(users);
 });
 
 
-router.post('/', async(req: any,res: any)=>{
-    const {username} = req.body;
-    
-    const userExists = await user.findUnique({
-        where:{
-            username
-        },select: {
-            username: true
-        }
-    })
 
-});
+router.post(
+  "/",
+  [    
+    check("username","username must be at least six characters").isLength({
+        min: 6,
+    }),
+    check("password", "Password must be at least six characters").isLength({
+      min: 6,
+    }),
 
+  ],
+  async (req: any, res: any) => {
+    const errors = validationResult(req);
+    const { username, password } = req.body;
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const addUserIfNotExists = await user.upsert({
+      where: {
+        username,
+      },
+      update: {},
+      create: {
+        username,
+        password,
+      },
+    });
+    return res.json({
+        "response": 200,
+        "message": "user added"
+    });
+  }
+);
 
 module.exports = router;
