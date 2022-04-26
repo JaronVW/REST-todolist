2da60d891287691bd8ad50446a2c256cf7d97030
Jaron van Well <49275554+JaronVW@users.noreply.github.com>
2022-04-26 14:33:04 +0200

    changed file naming convention

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ app.get("/", (req: any, res: any) => {
   res.send("<h1> Prisma API</h1>");
 });
 
-app.use("/api/users", require("./routes/user"));
-app.use("/api/notes", require("./routes/note"));
-app.use("/api/todos", require("./routes/todo"));
-app.use("/api/tasks", require("./routes/task"));
+app.use("/api/users", require("./routes/user.route"));
+app.use("/api/notes", require("./routes/note.route"));
+app.use("/api/todos", require("./routes/todo.route"));
+app.use("/api/tasks", require("./routes/task.route"));
 
 app.listen(port, () => {
   console.log(`app live on http://localhost:${port}`);
